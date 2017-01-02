9af71f786c05b21072a6af61b35753d7acbd734f
Qwal <qwalski@gmail.com>
2017-01-02 10:14:53 +0100

    Form submission + error handling

--- src/components/EventDetails.vue
+++ src/components/EventDetails.vue
@@ -4,20 +4,22 @@
       <h1 class="heading">Rejestracja</h1>
       <h2 class="title">{{ event.title }}</h2>
     </div>
-    <form v-on:submit.prevent="onSubmit">
+    <div class="success" v-if="successMessage">{{successMessage}}</div>
+    <div class="error" v-if="errorMessage">{{errorMessage}}</div>
+    <form v-on:submit.prevent="onSubmit" v-if="!formSent">
       <template v-for="field in sorted(event.fields)">
         <template v-if="field.type === 'text'">
-          <mdl-textfield :floating-label="field.label" :v-model="field.name" :required="field.required"></mdl-textfield>
+          <mdl-textfield :floating-label="field.label" v-model="form[field.name]" :required="field.required"></mdl-textfield>
         </template>
         <template v-if="field.type === 'email'">
-          <mdl-textfield :floating-label="field.label" :v-model="field.name" type="email" :required="field.required"></mdl-textfield>
+          <mdl-textfield :floating-label="field.label" v-model="form[field.name]" type="email" :required="field.required"></mdl-textfield>
         </template>
         <template v-if="field.type === 'checkbox' && !field.values">
-          <mdl-checkbox :v-model="field.name" :value="false">{{field.label}}</mdl-checkbox>
+          <mdl-checkbox v-model="form[field.name]" :value="false">{{field.label}}</mdl-checkbox>
         </template>
         <div class="multichoice" v-if="field.type === 'checkbox' && field.values">
           <label>{{field.label}}</label>
-          <mdl-checkbox v-for="val in toArray(field.values)" :v-model="field.name" :value="false">{{val}}</mdl-checkbox>
+          <mdl-checkbox v-for="(val, n) in toArray(field.values)" :val="val" :id="indexId(n)" type="checkbox" v-model="form[field.name]">{{val}}</mdl-checkbox>
         </div>
       </template>
       <mdl-button class="mdl-js-ripple-effect submit-btn" colored raised>Zapisz się</mdl-button>
@@ -26,7 +28,6 @@
 </template>
 
 <script>
-import find from 'lodash/find';
 import sortBy from 'lodash/sortBy';
 import { db } from '../firebase';
 
@@ -35,6 +36,12 @@ export default {
   data() {
     return {
       event: null,
+      errorMessage: null,
+      successMessage: null,
+      formSent: false,
+      form: {
+        interests: [], // TODO how to make multiple checkbox work without this default value
+      },
     };
   },
   methods: {
@@ -44,17 +51,31 @@ export default {
     toArray(string) {
       return string.split(',');
     },
+    indexId(num) {
+      return `id-${num.toString()}`;
+    },
     onSubmit() {
-      return true;
+      const self = this;
+
+      db.ref(`events/${this.event.key}/attendees`).push(this.form, (error) => {
+        if (error) {
+          self.errorMessage = error.code;
+          return;
+        }
+
+        self.successMessage = 'Dziękujemy za rejestrację!';
+        self.formSent = true;
+      });
     },
   },
   created() {
-    const events = db.ref('events');
     const selectedSeoSlug = this.$route.params.seoSlug;
     const self = this;
+    const eventRef = db.ref('events').orderByChild('seoSlug').equalTo(selectedSeoSlug);
 
-    events.on('value', (snapshot) => {
-      self.event = find(snapshot.val(), item => item.seoSlug === selectedSeoSlug);
+    eventRef.on('child_added', (snapshot) => {
+      self.event = snapshot.val();
+      self.event.key = snapshot.key;
     });
   },
 };
@@ -86,6 +107,21 @@ export default {
     margin-top: 15px;
     display: block;
     width: 100%;
+  }
+
+  .success, .error {
+    margin-top: 15px;
+    border-radius: 3px;
+    padding: 15px;
+    color: white;
+  }
+
+  .success {
+    background-color: forestgreen;
+  }
+
+  .error {
+    background-color: #ff5722;
   }
 
 </style>
