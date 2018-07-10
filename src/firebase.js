56e85081274b9fd711369c894e51220d32a9de5f
PGS-SOFT\ydubinskiy <ydubinskiy@pgs-soft.com>
2018-07-10 07:46:02 +0200

    Added service worker and firebase messaging

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,4 @@
-import * as firebase from 'firebase';
+import firebase from 'firebase';
 // Initialize Firebase
 const firebaseConfig = {
   apiKey: 'AIzaSyCzPlXBWNmzh-p0R9xhXS4GAVRdyPUsIvc',
--- src/main.js
+++ src/main.js
@@ -4,12 +4,30 @@ import VueMdl from 'vue-mdl';
 import App from './App.vue';
 import router from './router';
 import store from './store/index';
+import { messaging } from './firebase';
+import { actionTypes as userAction } from './store/modules/user';
 import './registerServiceWorker';
 import { sync } from 'vuex-router-sync';
 
 import 'material-design-lite/material.min';
 
 sync(store, router);
+
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.ready.then((reg) => {
+    messaging.useServiceWorker(reg);
+    messaging.requestPermission()
+      .then(() => messaging.getToken())
+      .then((token) => {
+        store.dispatch(userAction.TOKEN_RECEIVED, token);
+      })
+      .catch(() => {
+        store.dispatch(userAction.TOKEN_DENIED);
+      });
+  });
+} else {
+  store.dispatch(userAction.TOKEN_DENIED);
+}
 
 Vue.config.productionTip = false;
 
